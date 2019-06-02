c2ef036e844ea75ed20b201f900ac68a2d37f763
Jason Shin <visualbbasic@gmail.com>
2019-06-02 21:16:28 +1000

    refactor(lambda): removing extra spaces

--- src/lambda_example.rs
+++ src/lambda_example.rs
@@ -1,6 +1,5 @@
 #[test]
 fn lambdas() {
-
     fn  increment(i: i32) -> i32 { i + 1 }
     let closure_annotated = |i: i32| { i + 1 };
     let closure_inferred = |i| i + 1;
@@ -12,5 +11,4 @@ fn lambdas() {
     assert_eq!(inc, 4);
     assert_eq!(ca, 4);
     assert_eq!(ci, 4);
-
 }
