f81153d32c815397030d14f008e43eb6ac647751
Jason Shin <visualbbasic@gmail.com>
2019-06-23 13:48:28 +1000

    refactor(Lens): S as reference refactor

--- src/lens_example.rs
+++ src/lens_example.rs
@@ -1,7 +1,10 @@
-trait Lens<S, A> {
-    fn over(s: S, f: &Fn(A) -> A) -> A;
-    fn get(s: S) -> A;
-    fn set(a: A, s: S) -> S;
+trait Lens<S,    A> {
+    fn over(s: &S, f: &Fn(A) -> A) -> S {
+        let result: A = f(Self::get(s));
+        return Self::set(result, &s);
+    }
+    fn get(s: &S) -> A;
+    fn set(a: A, s: &S) -> S;
 }
 
 #[derive(Debug, PartialEq, Clone)]
@@ -13,20 +16,15 @@ struct Person {
 struct PersonNameLens;
 
 impl Lens<Person, String> for PersonNameLens {
-    fn over(s: Person, f: &Fn(String) -> String) -> String {
-        return f(Self::get(s));
+    fn get(s: &Person) -> String {
+        return s.name.to_string();
     }
 
-    fn get(s: Person) -> String {
-        return s.name;
-    }
-
-    fn set(a: String, s: Person) -> Person {
+    fn set(a: String, s: &Person) -> Person {
         return Person {
             name: a,
         }
     }
-
 }
 
 #[test]
@@ -35,11 +33,14 @@ fn lens_example() {
         name: "Jason".to_string(),
     };
 
-    let name = PersonNameLens::get(e1.clone());
-    let e2 = PersonNameLens::set("John".to_string(), e1.clone());
+    let name = PersonNameLens::get(&e1);
+    let e2 = PersonNameLens::set("John".to_string(), &e1);
     let expected = Person {
         name: "John".to_string()
     };
+
+    let e3 = PersonNameLens::over(&e1, &|x: String| x.to_uppercase());
     assert_eq!(name, e1.name);
     assert_eq!(e2, expected);
+    assert_eq!(e3, Person { name: "JASON".to_string() });
 }
