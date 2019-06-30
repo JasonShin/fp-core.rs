fd35249e189b43bee13bc437d0f89d0d70ec2e39
Jason Shin <visualbbasic@gmail.com>
2019-06-30 16:52:41 +1000

    feature(applicative): adding applicative code

--- src/applicative_example.rs
+++ src/applicative_example.rs
@@ -1,5 +1,30 @@
-use crate::functor_example::Functor;
+use crate::functor_example::{Functor, HKT};
 
-trait Apply<C, D> : Functor<C, D> {
-
+trait HKT3<A, B, C> {
+    type Target2;
 }
+
+trait Apply<A, F, B> : Functor<A, B> + HKT3<A, F, B>
+    where F: FnOnce(A) -> B,
+{
+    fn ap(self, f: <Self as HKT3<A, F, B>>::Target2) -> <Self as HKT<A, B>>::Target;
+}
+
+impl<A, B, C> HKT3<A, B, C> for Option<A> {
+    type Target2 = Option<B>;
+}
+
+impl<A, F, B> Apply<A, F, B> for Option<A>
+    where F: FnOnce(A) -> B,
+{
+    fn ap(self, f: Self::Target2) -> Self::Target {
+        self.and_then(|v| f.map(|z| z(v)))
+    }
+}
+
+trait Applicative<A, F, B> : Apply<A, F, B>
+    where F: FnOnce(A) -> B,
+{
+    fn of(a: A) -> <Self as HKT<A, B>>::Target;
+}
+
