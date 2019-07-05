e7ed36d0aaa8cc3fe0b77ce923833044528abe15
Jason Shin <visualbbasic@gmail.com>
2019-07-06 00:01:29 +1000

    feature(comonad): adding a comonad example

--- src/comonad_example.rs
+++ src/comonad_example.rs
@@ -1,18 +1,34 @@
-/*
-trait Comonad<A, B>: Extend<A, B>
-{
-    fn extract(self) -> A;
+use crate::functor_example::{Functor, HKT};
 
-    // extend should already be provided from `Extend`
-    fn extend(self, f: F) -> HKT1<B>
+trait Extend<A, B>: Functor<A, B> + Sized {
+    fn extend<W>(self, f: W) -> <Self as HKT<A, B>>::Target
     where
-        F: Fn(Self) -> B;
+        W: FnOnce(Self) -> B;
 }
-*/
+
+impl<A, B> Extend<A, B> for Option<A> {
+    fn extend<W>(self, f: W) -> Self::Target
+    where
+        W: FnOnce(Self) -> B,
+    {
+        self.map(|x| f(Some(x)))
+    }
+}
+
+trait Extract<A> {
+    fn extract(self) -> A;
+}
+
+impl<A> Extract<A> for Option<A> {
+    fn extract(self) -> A {
+        self.unwrap() // is there a better way to achieve this?
+    }
+}
+
+trait Comonad<A, B>: Extend<A, B> + Extract<A> {}
 
 #[test]
 fn comonad_test() {
-    // Check the readme to understand comonad
-    // Skipping implementation detail as it require HKT and various other components
-    assert!(true);
+    let z = Some(1).extend(|x| x.extract() + 1);
+    assert_eq!(z, Some(2));
 }
