97d17f0ee5aad382b572f5a29c79f23ce39dd83a
Jason Shin <visualbbasic@gmail.com>
2019-08-05 12:06:04 +1000

    refactor(hkt): moving HKT3 to fp-core

--- fp-core/src/hkt.rs
+++ fp-core/src/hkt.rs
@@ -7,3 +7,11 @@ impl<A, B> HKT<A, B> for Option<A> {
     type URI = Self;
     type Target = Option<B>;
 }
+
+pub trait HKT3<A, B, C> {
+    type Target2;
+}
+
+impl<A, B, C> HKT3<A, B, C> for Option<A> {
+    type Target2 = Option<B>;
+}
