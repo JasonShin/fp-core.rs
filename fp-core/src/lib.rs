3dba9dd7d8b77eeccbfe7c06eb2a93865145dc02
Jason Shin <visualbbasic@gmail.com>
2019-08-05 15:06:35 +1000

    refactor(monad.rs): moving monad to monad.rs

--- fp-core/src/lib.rs
+++ fp-core/src/lib.rs
@@ -3,4 +3,5 @@ pub mod apply;
 pub mod chain;
 pub mod functor;
 pub mod hkt;
+pub mod monad;
 pub mod pure;
--- fp-examples/src/monad_example.rs
+++ fp-examples/src/monad_example.rs
@@ -1,15 +1,6 @@
-use fp_core::applicative::*;
 use fp_core::chain::*;
-use fp_core::hkt::HKT;
+use fp_core::monad::*;
 use fp_core::pure::*;
-
-pub trait Monad<A, F, B>: Chain<A, B> + Applicative<A, F, B>
-where
-    F: FnOnce(A) -> B,
-{
-}
-
-impl<A, F, B> Monad<A, F, B> for Option<A> where F: FnOnce(A) -> B {}
 
 #[test]
 fn monad_example() {
