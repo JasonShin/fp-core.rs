1ae21cea994fef0054944c4b72342a4054a5dedf
Heman Gandhi <hemangandhi@gmail.com>
2019-08-10 14:56:47 -0400

    better semi

--- fp-core/src/semigroup.rs
+++ fp-core/src/semigroup.rs
@@ -1,3 +1,15 @@
 use std::ops::Add;
 
-pub trait Semigroup<M>: Add<M> {}
+pub trait Semigroup<T> {
+    fn combine(self, other: T) -> T;
+}
+
+impl<T, I> Semigroup<T> for I
+where
+    I: Add<T, Output = T>,
+    Self: Into<T>,
+{
+    fn combine(self, other: T) -> T {
+        self.add(other)
+    }
+}
