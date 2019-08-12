8ef3da1ecbfd809e3004680581eb515288432537
Jason Shin <visualbbasic@gmail.com>
2019-08-12 15:06:07 +1000

    wip(traverse.rs): working on initial traverse, trying to refactor HKT3

--- fp-core/src/traverse.rs
+++ fp-core/src/traverse.rs
@@ -1,1 +1,9 @@
-trait Traverse<F, G> {}
+use crate::functor::Functor;
+use crate::applicative::Applicative;
+use crate::hkt::{HKT3, HKT};
+
+trait Traverse<A, B, F, G>: Functor<A, B> + Applicative<A, F, B> {
+    fn traverse<FA>(&self) -> <Self as HKT3<G, F, B>>::Target2
+    where
+        FA: FnOnce(A) -> <Self as HKT<G, B>>::Target;
+}
