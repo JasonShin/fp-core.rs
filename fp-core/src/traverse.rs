f2303031af63da1b06540cb4f0fcdc0f9067cdbc
Jason Shin <visualbbasic@gmail.com>
2019-08-13 11:50:50 +1000

    feature(traverse.rs): half correct traverse signature

--- fp-core/src/traverse.rs
+++ fp-core/src/traverse.rs
@@ -1,1 +1,9 @@
-trait Traverse<F, G> {}
+use crate::applicative::Applicative;
+use crate::functor::Functor;
+use crate::hkt::{HKT, HKT3};
+
+trait Traverse<A, B, F, G>: HKT<G, B> + HKT3<G, F, B> {
+    fn traverse<FB>(&self, f: FB) -> <Self as HKT3<G, F, B>>::Target2
+    where
+        FB: FnOnce(A) -> <Self as HKT<G, B>>::Target;
+}
