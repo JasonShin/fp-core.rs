c0f6b87cef917e7d4dea871baebe152f1be50108
Heman Gandhi <hemangandhi@gmail.com>
2019-08-15 17:02:48 -0400

    clear up ambiguity

--- fp-core/src/apply.rs
+++ fp-core/src/apply.rs
@@ -1,7 +1,7 @@
 use crate::functor::Functor;
 use crate::hkt::HKT;
 
-type Applicator<B, S: HKT<B>> = <S as HKT<Box<Fn(S::Current) -> B>>>::Target;
+type Applicator<B, S: HKT<B>> = <S as HKT<Box<Fn(<S as HKT<B>>::Current) -> B>>>::Target;
 
 pub trait Apply<B>: Functor<B> + HKT<Box<Fn(<Self as HKT<B>>::Current) -> B>> {
     fn ap(self, f: Applicator<B, Self>) -> <Self as HKT<B>>::Target;
