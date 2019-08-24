db1d950327c2eca3bd196a8151fc3ae9af8becfc
Jason Shin <visualbbasic@gmail.com>
2019-08-24 15:43:46 +1000

    refactor(chain.rs): cleanup verbose Self::Target

--- fp-core/src/chain.rs
+++ fp-core/src/chain.rs
@@ -1,15 +1,15 @@
 use crate::hkt::HKT;
 
 pub trait Chain<B>: HKT<B> {
-    fn chain<F>(self, f: F) -> <Self as HKT<B>>::Target
+    fn chain<F>(self, f: F) -> Self::Target
     where
-        F: FnOnce(<Self as HKT<B>>::Current) -> <Self as HKT<B>>::Target;
+        F: FnOnce(<Self as HKT<B>>::Current) -> Self::Target;
 }
 
 impl<A, B> Chain<B> for Option<A> {
     fn chain<F>(self, f: F) -> Self::Target
     where
-        F: FnOnce(A) -> <Self as HKT<B>>::Target,
+        F: FnOnce(A) -> Self::Target,
     {
         self.and_then(f)
     }
