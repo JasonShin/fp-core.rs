f3a04f44da9c1649c270d6028aeff074dd502209
Jason Shin <visualbbasic@gmail.com>
2019-12-24 14:19:33 +1100

    refactor(apply.rs): adding dyn on Box trait also updating README.md to contain travis badge

--- fp-core/src/apply.rs
+++ fp-core/src/apply.rs
@@ -1,9 +1,9 @@
 use crate::functor::Functor;
 use crate::hkt::HKT;
 
-type Applicator<B, S: HKT<B>> = <S as HKT<Box<Fn(<S as HKT<B>>::Current) -> B>>>::Target;
+type Applicator<B, S: HKT<B>> = <S as HKT<Box<dyn Fn(<S as HKT<B>>::Current) -> B>>>::Target;
 
-pub trait Apply<B>: Functor<B> + HKT<Box<Fn(<Self as HKT<B>>::Current) -> B>> {
+pub trait Apply<B>: Functor<B> + HKT<Box<dyn Fn(<Self as HKT<B>>::Current) -> B>> {
     fn ap(self, f: Applicator<B, Self>) -> <Self as HKT<B>>::Target;
 }
 
