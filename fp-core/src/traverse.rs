8763fe57f9a7aa39c64456a824b2d72d6ecaaa15
Heman Gandhi <hemangandhi@gmail.com>
2020-01-11 15:24:32 -0500

    start sequence impl

--- fp-core/src/traverse.rs
+++ fp-core/src/traverse.rs
@@ -7,40 +7,29 @@ use std::fmt::Debug;
 
 // Re-statement of the Haskell definition
 // (http://hackage.haskell.org/package/base-4.12.0.0/docs/Data-Traversable.html)
-pub trait Traversable<B>: Functor<B> + Foldable<B>
-{
-    // A is the source type, F the functor, and AFB a natural transformation
-    // from A into F<B>. Pursuant to Cats in Scala (https://www.scala-exercises.org/cats/traverse),
-    // A would be a User, F a future, and Self would be a list. AFB would be the sort of "user
-    // promise" function in the example.
-    fn traverse(&self, traverser: Box<AFB>) -> HktInHkt<F, Self, B>
-    // (Self is a HKT<B> so this works out)
-    where
-        AFB: Fn(<Self as HKT<B>>::Current) -> <F as HKT<B>>::Target;
-}
-
-// Re-statement of the Haskell function
-// (http://hackage.haskell.org/package/base-4.12.0.0/docs/Data-Traversable.html)
-pub fn sequenceA<T, F, A>(wrong_order: HktInHkt<T, F, A>) -> HktInHkt<F, T, A>
+// that uses sequence as the basis for traverse, that is:
+//     class (Functor t, Foldable t) => Traverse t where
+//       sequence::(Applicative f) => t (f b) -> f (t b)
+//
+// Unfortunately, Rust doesn't handle a "for all" in the same way, so to Rust,
+// we must know that T is functorial over F<B> and just B and F is Applicative
+// for B and T<B>.
+pub trait Traversable<B, F>:
+    Functor<B> + Foldable<B> + Functor<<F as HKT<B>>::Target> + Foldable<<F as HKT<B>>::Target>
 where
-    T: Traversable<A, F> + HKT<<F as HKT<A>>::Target>,
-    F: Applicative<A> + Applicative<<T as HKT<A>>::Target>,
-    HktInHkt<T, F, A>: Traversable<A, F>
+    F: Applicative<B> + Applicative<<Self as HKT<B>>::Target>,
 {
-    Traversable::<A, F>::traverse(&wrong_order, Box::new(|x| x))
+    fn sequence(tfb: HktInHkt<Self, F, B>) -> HktInHkt<F, Self, B>;
 }
 
 impl<A, B, F> Traversable<B, F> for Option<A>
 where
     F: Applicative<B> + Applicative<Option<B>>,
 {
-    fn traverse<AFB>(&self, traverser: Box<AFB>) -> HktInHkt<F, Self, B>
-    where
-        AFB: Fn(A) -> <F as HKT<B>>::Target,
-    {
-        match &self {
+    fn sequence(tbf: HktInHkt<Self, F, B>) -> HktInHkt<F, Self, B> {
+        match tbf {
             Option::None => F::of(Option::None),
-            Option::Some(a) => F::of(Option::Some(traverser(*a))),
+            Option::Some(fb) => F::of
         }
     }
 }
