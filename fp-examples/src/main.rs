b93d3661062e71868e039c427492f162db206729
Vitalij Zadneprovskij <vitalij_zad@libero.it>
2020-08-31 12:47:29 +0200

    Cleaned up fp-examples

--- fp-examples/src/main.rs
+++ fp-examples/src/main.rs
@@ -3,38 +3,8 @@ extern crate partial_application;
 #[macro_use]
 extern crate fp_core;
 
-pub mod adt_example;
-mod anamorphism_example;
-mod applicative_example;
-mod arity_example;
-mod catamorphism_example;
-mod closure_example;
-mod comonad_example;
-mod continuation_example;
-mod contracts_example;
-mod currying_example;
-mod empty_example;
-mod endomorphism_example;
-mod foldable_example;
-mod function_composition_example;
-mod functor_example;
-mod hof_example;
-mod idempotent_example;
-mod isomorphism_example;
-mod lambda_example;
-mod lens_example;
-mod monad_example;
-mod monoid_example;
-mod option_example;
-mod partial_application_example;
-mod pointed_functor_example;
-mod predicate_example;
-mod purity_example;
-mod referential_transparency_example;
-mod semigroup_example;
-mod setoid_example;
-mod side_effects_example;
-mod type_signature_example;
+mod fp_core;
+mod fp_jargon;
 
 fn main() {
     println!("Welcome to fp-core!");
